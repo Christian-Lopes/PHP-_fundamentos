34dcffe72cc70a98008cf3c9fed93477fbbc29c0
Christian-Lopes <christianpnn_@hotmail.com>
2021-04-08 16:36:18 -0300

    Meu primeiro projeto PHP

---
+++ primeiro_projeto_php/dados/cursos.php
@@ -0,0 +1,31 @@
+<?php
+
+    function cursos() {
+        return [
+            "PHP" => [
+                "nome_curso" => "PHP fundamentos",
+                "versao_ferramenta" => 7.4,
+                "carga_horaria" => 120,
+                "status" => true
+            ],
+            "Java" => [
+                "nome_curso" => "Java fundamentos",
+                "versao_ferramenta" => 15,
+                "carga_horaria" => 160,
+                "status" => true
+            ],
+            "C++" => [
+                "nome_curso" => "C++ fundamentos",
+                "versao_ferramenta" => 10,
+                "carga_horaria" => 160,
+                "status" => true
+            ],
+            "C#" => [
+                "nome_curso" => "C# fundamentos",
+                "versao_ferramenta" => 7.4,
+                "carga_horaria" => 120,
+                "status" => false
+            ]
+            ];
+    
+    }
