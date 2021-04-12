0b709bfd2f1c9a6f2909ade358af41b52f24403e
Christian-Lopes <christianpnn_@hotmail.com>
2021-04-12 18:04:20 -0300

    Implementando projeto Notification com Composer

---
+++ notification/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f32e55f2181bc0da860de6d16437896a62de38bf',
+    'name' => 'chris/notification',
+  ),
+  'versions' => 
+  array (
+    'chris/notification' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f32e55f2181bc0da860de6d16437896a62de38bf',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '050d430203105c27c30efd1dce7aa421ad882d01',
+    ),
+  ),
+);
